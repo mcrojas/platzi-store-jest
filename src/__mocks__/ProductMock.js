e509d7b98cafd3e5056cdf1c65c5339e907d0456
Mary Rojas <mary.rojas@square1.io>
2021-04-08 12:18:06 -0400

    Travis config

---
+++ src/__mocks__/ProductMock.js
@@ -0,0 +1,5 @@
+const ProductMock = {
+
+}
+
+export default ProductMock;
---
+++ src/__test__/utils/getData.test.js
@@ -0,0 +1,15 @@
+import getData from '../../utils/getData';
+
+describe('Fetch API', () => {
+    beforeEach(() => {
+        fetch.resetMocks();
+    });
+    test('Llamar una API y retornar datos', () => {
+        fetch.mockResponseOnce(JSON.stringify({ 'data': '123'}));
+        getData('https://www.google.com/')
+        .then((response) => {
+            expect(response.data).toEqual('123');
+        });
+        expect(fetch.mock.calls[0][0]).toEqual('https://www.google.com/')
+    })
+})
